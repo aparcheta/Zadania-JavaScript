58b0d468e6337d763afec65f707605eacebe2898
aparcheta <parcheta.anna@gmail.com>
2022-10-01 20:46:22 +0200

    dodanie zadania 29

---
+++ 29-js-suma-zmiennych/js/main.js
@@ -0,0 +1,9 @@
+let firstNumber = 8
+let secondNumber = 2
+function sum (firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+  }
+
+  let wynik = sum(firstNumber, secondNumber);
+  console.log(wynik);
---
+++ 30-js-dodawanie-tekstu/js/main.js
@@ -0,0 +1,4 @@
+let object = document.querySelector('p');
+object.textContent = 'Akademia 108';
+
+console.log(object)
