b6a9172e363c3176586929578492b31b77995b01
aparcheta <parcheta.anna@gmail.com>
2022-10-30 20:08:10 +0100

    dodanie zadania 1-animacja jQuery

---
+++ jQuery-1-animacja-jquery/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    const square = $('.red');
+    $('#animate').click(function(){
+        square.animate({height: 100, width: 100, marginLeft: 100}, 3000, function(){
+            $(this).animate({backgroundColor: 'blue'}, 5000, function(){
+                $('h2').text('Animacja zakończona');
+
+            });
+            
+
+
+        });
+
+
+    });
+
+
+
+
+});
