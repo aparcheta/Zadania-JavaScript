b317c7899a239638580bec4288e781b134a96a18
aparcheta <parcheta.anna@gmail.com>
2022-10-30 11:55:08 +0100

    dodanie zadania 4-accordion-jquery

--- cwiczenia/js/script.js
+++ cwiczenia/js/script.js
@@ -80,3 +80,54 @@ for(let auto in cars){
 for(let auto in cars){
     console.log(`Nazwa pola: ${cars.model}`);
 }
+
+
+// tworzysz funkcję "popularity", która ma przyjmowac jeden parametr ->  tablicę.  Funkcja ma dodawac do elementów tablicy (ktore będą obiektami) dodatkowe pole "popularity" , któremu będzie nadawała losową liczbę.
+// hciaabym zebyś w kilku console.log'ach które mi przeslesz tutaj dostała się do określonych danych
+// 1. imię Marian
+// 2. hobby Krystiana (medytacja)
+// 3.  wszystkie hobby Łukasza
+// 4. ulubione jedzenie Uli( arbuz )
+// 5. ulubione jedzenie Uli( czekolada)
+// 6. 4. ulubione jedzenie Uli( cytryna)
+// tablica zwiera elementy tablicy
+// obiekt zawiera pola obiektu/właściwości/pary klucz - wartość
+
+
+
+
+let array = [
+
+  {
+      imie: "Ula",
+      nazwisko: "Jeziorna",
+      plec: "kobieta",
+      hobby: ["bieganie", "taniec", "malowanie"],
+      ulubioneJedzenie: {owoce: [{kwasne: ['wiśnie', 'cytryna', 'kiwi'], slodkie: ['melon', 'arbuz', 'truskawki']}], warzywa: ['sałata', 'pomidory', 'marchewka'], slodycze: ['ciasta', 'czekolada']}
+  },
+  {
+      imie: "Krystian",
+      nazwisko: "Dziopa",
+      plec: "mężczyzna",
+      hobby: ["programowanie", "medytacja"]
+  },
+  {
+      imie: "Łukasz",
+      nazwisko: "Badocha",
+      plec: "mężczyzna",
+      hobby: ["rower", "góry", "bieganie"]
+  },
+  {
+      imie: "Marian",
+      nazwisko: "Drągalski",
+      plec: "mężczyzna",
+      hobby: ["wspinanie", "rower", "karate", "gotowanie"]
+  },
+];
+
+console.log(array[3].imie);
+console.log(array[1].hobby[1]);
+console.log(array[2].hobby);
+console.log(array[0].ulubioneJedzenie.owoce[0].slodkie[1]);
+console.log(array[0].ulubioneJedzenie.slodycze[1]);
+console.log(array[0].ulubioneJedzenie.owoce[0].kwasne[1]);
